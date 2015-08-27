32a7597b8ad59c1364be78faf101d2e5fa082f5c
doanhpham <doanhpham.it@gmail.com>
2015-08-27 15:33:53 +0700

    Update HTML + CSS

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'jlitreal_db');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'doanh');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
