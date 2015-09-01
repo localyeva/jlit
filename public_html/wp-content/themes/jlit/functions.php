a3ee740d028679a941acbba94b110c37f5ec32d0
doanhpv <doanhpv@localhost>
2015-09-01 14:38:33 +0700

    change design

--- public_html/wp-content/themes/jlit/functions.php
+++ public_html/wp-content/themes/jlit/functions.php
@@ -21,3 +21,27 @@ add_action( 'wp_enqueue_scripts', 'jlit_scripts' );
 
 //Nav menu
 register_nav_menu('primary', 'Primary Menu');
+
+/**
+ * Register widget area.
+ *
+ *JLIT 1.0
+ *
+ */
+function jlit_widgets_init() {
+	register_sidebar( array(
+			'name'          => __( 'Widget Area', 'jlit' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in your sidebar.', 'jlit' ),
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'jlit_widgets_init' );
+/*
+ * Khởi tạo widget item
+ */
+
+
