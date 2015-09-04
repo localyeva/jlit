9626a88adf5363639b5e6d3fed7b6eb112c01ea8
doanhpv <doanhpv@localhost>
2015-09-04 10:43:45 +0700

    fix css

--- public_html/wp-content/plugins/widget-plugin/helloworld.php
+++ public_html/wp-content/plugins/widget-plugin/helloworld.php
@@ -32,7 +32,16 @@ class Helloworld_Widget extends WP_Widget {
 	 * Tạo form option cho widget
 	 */
 	function form( $instance ) {
-
+		//Biến tạo các giá trị mặc định trong form
+		$default = array(
+		        'title' => 'Tên của bạn'
+		);
+		//Gộp các giá trị trong mảng $default vào biến $instance để nó trở thành các giá trị mặc định
+		$instance = wp_parse_args( (array) $instance, $default);
+		//Tạo biến riêng cho giá trị mặc định trong mảng $default
+		$title = esc_attr( $instance['title'] );
+		//Hiển thị form trong option của widget
+		echo "Nhập tiêu đề <input class=".'widefat'." type=".'text'." name=".$this->get_field_name('title')."' value='".$title."' />";
 	}
 
 	/**
@@ -40,7 +49,11 @@ class Helloworld_Widget extends WP_Widget {
 	 */
 
 	function update( $new_instance, $old_instance ) {
-
+		parent::update( $new_instance, $old_instance );
+		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
 	}
 
 	/**
@@ -48,7 +61,21 @@ class Helloworld_Widget extends WP_Widget {
 	 */
 
 	function widget( $args, $instance ) {
-
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		
+		echo $before_widget;
+		
+		//In tiêu đề widget
+		echo $before_title.$title.$after_title;
+		
+		// Nội dung trong widget
+		
+		echo "ABC XYZ";
+		
+		// Kết thúc nội dung trong widget
+		
+		echo $after_widget;
 	}
 
 }
