e50f78deec9d9d7ef8ed24ab37c49b0ea13aa2eb
haudv <haudv@evolableasia.vn>
2015-09-17 11:30:19 +0700

    fix top image

--- public_html/wp-content/themes/virtue/templates/slider.php
+++ public_html/wp-content/themes/virtue/templates/slider.php
@@ -1,20 +1,16 @@
-		<div id="slider">
-			<div id="carousel-example-generic" class="container-fluid wrapper" data-ride="carousel">
-				<div class="carousel-inner" role="listbox">
-					<div class="text-center item active">
-						<img class="img-responsive" style="width: 100%" src="<?php bloginfo('template_url'); ?>/assets/img/vir/banner.png" alt="">
-						 <div class="carousel-caption format-stand">
-					        <h4>Kì thi năng lực nhật ngữ cho chuyên ngành IT</h4>
-							<p>Japanse literacy & IT Test(JLIT)</p>
-							<a href="#form-register">Đăng ký miễn phí</a><br>
-							<img class="" src="<?php bloginfo('template_url'); ?>/assets/img/vir/arrow.png" alt="">
-					      </div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div id="note" class="container-fluid">
-			<div>
-				<img class="img-responsive" style="width: 100%" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/vir/note.png">
-			</div>
-		</div>
+<div class="bg head-img" id="fixed-sequence" style="background-size: cover;">
+    <div class="row bg-height">
+        <div class="col-xs-6 col-md-4 text-left text-left-head">
+            <h4>Kì thi năng lực nhật ngữ cho chuyên ngành IT</h4>
+            <p>Japanse literacy & IT Test(JLIT)</p>
+            <a href="#form-register">Đăng ký miễn phí</a><br>
+            <img class="" src="<?php bloginfo('template_url'); ?>/assets/img/vir/arrow.png" alt="">
+        </div>                   
+    </div><!-- grid -->  
+</div>
+
+<div id="note" class="container-fluid">
+    <div>
+        <img class="img-responsive" style="width: 100%" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/vir/note.png">
+    </div>
+</div>
