2e9cadc8182ebdc7f57abb10ab93e30072bd055e
duych <duych@cpu-duych.local>
2015-12-22 15:03:59 +0700

    change flag position

--- public_html/wp-content/themes/virtue/templates/footer.php
+++ public_html/wp-content/themes/virtue/templates/footer.php
@@ -4,14 +4,14 @@
 			<div class="footer-top">
 				<div class="container">
 					<div class="row">
-						<div class="col-sm-4 pad-t-70 padd-cent">
+						<div class="col-sm-4 pad-t-70 padd-cent float-l">
 							<img class="img-responsive" alt="" src="<?php bloginfo('template_url'); ?>/assets/img/vir/vnw.png">
 						</div>
-						<div class="col-sm-4 txt-cent">
+						<div class="col-sm-4 txt-cent float-l">
 							<img class="img-responsive"  alt="" src="<?php bloginfo('template_url'); ?>/assets/img/vir/evo.png">
 							<p>Email: jlit@evolable.asia</p>
 						</div>
-						<div class="col-sm-4 text-right">
+						<div class="col-sm-4 text-right float-l">
 							<div class="facebook">
 								<div id="fb-root"></div>
 								<script>(function(d, s, id) {
